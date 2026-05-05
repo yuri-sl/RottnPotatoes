dbfb352555a64d770cb0a9cca78679683fb5b32c
Yuri Santana <yuri03ysl@gmail.com>
2026-05-04 23:06:41 -0300

    Correcoes iniciais nos testes

--- features/movies/create.feature
+++ features/movies/create.feature
@@ -6,7 +6,36 @@ Feature: Create Movie
 Scenario: create a new movie
   Given I am on the index page
   When I click the add Movie button
-  Then I should be redirected to new movie page
+  Then I should be redirected to add new movie page
   And I should input data into the form fields
   And I should press the Create Movie button
   And I should be redirected to index page
+
+  Scenario: View movie list after adding 2 movies (Imperative)
+    Given I am on the index page
+    When I click "Novo Filme"
+    Then I should be redirected to add movie page
+    When I fill in "Título" with "Zorro"
+    And I fill in "Data de Lançamento" with "1998-07-17"
+    And I select "PG" from "Classificação"
+    And I press "Adicionar filme"
+    Then I should be on the RottenPotatoes home page
+    When I click "Novo Filme"
+    Then I should be redirected to add movie page
+    When I fill in "Título" with "Apocalypse Now"
+    And I fill in "Data de Lançamento" with "1979-08-15"
+    And I select "R" from "Classificação"
+    And I press "Adicionar filme"
+    Then I should be on the RottenPotatoes home page
+    When I click "Título"
+    Then I should see "Apocalypse Now" before "Zorro"
+
+
+
+Scenario: View movie list sorted alphabetically (Declarative)
+  Given the following movies exist:
+    | title          | rating | release_date |
+    | Zorro          | PG     | 1998-07-17   |
+    | Apocalypse Now | R      | 1979-08-15   |
+  When I visit the movie list sorted by title
+  Then I should see "Apocalypse Now" before "Zorro"
--- features/step_definitions/create_steps.rb
+++ features/step_definitions/create_steps.rb
@@ -8,7 +8,7 @@ When /I click the add Movie button/ do
   click_link('Novo Filme')
 end
 
-Then /I should be redirected to new movie page/ do
+Then /I should be redirected to add new movie page/ do
   expect(current_path).to eq('/movies/add')
 end
 
@@ -26,4 +26,52 @@ end
 
 And /I should be redirected to index page/ do
   visit('/movies')
+end
+
+
+When /I fill in "([^"]*)" with "([^"]*)"/ do |field, value|
+  fill_in field, with: value
+end
+
+# Clicar em um link pelo texto
+When /I click "([^"]*)"/ do |link|
+  click_link link
+end
+
+# Verificar se está na página de criar novo filme
+Then /I should be redirected to add movie page/ do
+  expect(current_path).to eq(movies_add_path)
+end
+
+# Verificar se está na home
+Then /I should be on the RottenPotatoes home page/ do
+  expect(current_path).to eq(movies_path)
+end
+
+# Verificar ordem dos filmes na página
+Then /I should see "([^"]*)" before "([^"]*)"/ do |first, second|
+  content = page.body
+  first_pos  = content.index(first)
+  second_pos = content.index(second)
+  expect(first_pos).to be < second_pos,
+                       "Expected '#{first}' to appear before '#{second}'"
+end
+
+
+Given /the following movies exist:/ do |table|
+  table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
+end
+
+When /I visit the movie list sorted by title/ do
+  visit movies_path(sort_by: 'title', sort_order: 'asc')
+end
+
+When /I select "([^"]*)" from "([^"]*)"/ do |value, field|
+  select value, from: field
+end
+
+When /I press "([^"]*)"/ do |button|
+  click_button button
 end
